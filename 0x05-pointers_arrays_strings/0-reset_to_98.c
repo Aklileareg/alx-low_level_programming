972a10d46fbce1c7c534b03337e4d6e4ca4fc1d2
Aklileareg <aklilearegahegn@gmail.com>
2022-03-21 23:23:42 +0100

    My commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98. [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D and updates the value it points to to 98.
+ * 
+ * @n: number.
+ * 
+ */
+
+void reset_to_98(int *n);
+(
+*n = 98;[A[D[D[D[D[D[D[{[B[B[C[C[C[C[C[C[C
+}
