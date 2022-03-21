d5fcaab15d99314809206f0c3ffea96c28f241e1
Aklileareg <aklilearegahegn@gmail.com>
2022-03-21 22:52:00 +0100

    My commit world

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _main_h_
+#define _main_h_
+
+void reset_to_989int *[D[D[D[D[([C[C[C[C[Cn);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
